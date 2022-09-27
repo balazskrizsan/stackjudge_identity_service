5722510e044b798cefeeeee71538b9d1be917eee
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-09-27 22:06:59 +0200

    Migration added back

--- IDS/Stackjudge Identity Server/Startup.cs
+++ IDS/Stackjudge Identity Server/Startup.cs
@@ -1,8 +1,11 @@
+using Duende.IdentityServer.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Stackjudge_Identity_Server.Data;
 
 namespace Stackjudge_Identity_Server;
 
@@ -68,42 +71,43 @@ public class Startup
 
     private void InitializeDatabase(IApplicationBuilder app)
     {
-        // Run only on db setup
-        // using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-        // {
-        //     serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
-        //     serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
-        //     serviceScope.ServiceProvider.GetRequiredService<AppDbContext>().Database.Migrate();
-        //
-        //     var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-        //     context.Database.Migrate();
-        //     if (!context.Clients.Any())
-        //     {
-        //         foreach (var client in Config.Clients)
-        //         {
-        //             context.Clients.Add(client.ToEntity());
-        //         }
-        //     
-        //         context.SaveChanges();
-        //     }
-        //
-        //     if (!context.IdentityResources.Any())
-        //     {
-        //         foreach (var resource in Config.IdentityResources)
-        //         {
-        //             context.IdentityResources.Add(resource.ToEntity());
-        //         }
-        //         context.SaveChanges();
-        //     }
-        //
-        //     if (!context.ApiResources.Any())
-        //     {
-        //         foreach (var resource in Config.ApiResources)
-        //         {
-        //             context.ApiResources.Add(resource.ToEntity());
-        //         }
-        //         context.SaveChanges();
-        //     }
-        // }
+        using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+        {
+            serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+            serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
+            serviceScope.ServiceProvider.GetRequiredService<AppDbContext>().Database.Migrate();
+
+            // Run only on db setup
+            //
+            //     var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            //     context.Database.Migrate();
+            //     if (!context.Clients.Any())
+            //     {
+            //         foreach (var client in Config.Clients)
+            //         {
+            //             context.Clients.Add(client.ToEntity());
+            //         }
+            //     
+            //         context.SaveChanges();
+            //     }
+            //
+            //     if (!context.IdentityResources.Any())
+            //     {
+            //         foreach (var resource in Config.IdentityResources)
+            //         {
+            //             context.IdentityResources.Add(resource.ToEntity());
+            //         }
+            //         context.SaveChanges();
+            //     }
+            //
+            //     if (!context.ApiResources.Any())
+            //     {
+            //         foreach (var resource in Config.ApiResources)
+            //         {
+            //             context.ApiResources.Add(resource.ToEntity());
+            //         }
+            //         context.SaveChanges();
+            //     }
+        }
     }
 }
